3a5ddf543a0ea59ee239b5f3b504b9c810a1548b
Abishek-077 <abishekxofficial987@gmail.com>
2025-04-08 17:16:19 +0545

    Working with MongoDb

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,39 +1,69 @@
 import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const app = express();
+let db; // Declare db
 
 const articleInfo = [
     { name: 'learn-node', upvotes: 0, comment: [] },
     { name: 'learn-react', upvotes: 0, comment: [] },
     { name: 'mongodb', upvotes: 0, comment: [] },
-]
-// Corrected: express.json() needs to be invoked as middleware
-app.use(express.json());  // This will parse incoming JSON requests
+];
 
-app.post('/api/articles/:name/upvote', (req, res) => {
-    const article = articleInfo.find(a => a.name === req.params.name);
-    article.upvotes += 1;
+app.use(express.json());
 
+async function connectToDB() {
+    const uri = 'mongodb://127.0.0.1:27017';
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+    await client.connect();
+    db = client.db('full-stack-react-db');
+}
+
+app.get('/api/articles/:name', async (req, res) => {
+    const { name } = req.params;
+    const article = await db.collection('articles').findOne({ name });
     res.json(article);
-
 });
 
-app.post('/api/articles/:name/comments', (req, res) => {
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    const { name } = req.params;
+
+    const result = await db.collection('articles').findOneAndUpdate(
+        { name },
+        { $inc: { upvotes: 1 } },
+        { returnDocument: 'after' }
+    );
+
+    res.json(result.value);
+});
+
+app.post('/api/articles/:name/comments', async (req, res) => {
     const { name } = req.params;
     const { postedBy, text } = req.body;
 
-    const article = articleInfo / find(a => a.name === name);
+    const newComment = { postedBy, text };
 
-    article.comment.push({
-        postedBy,
-        text,
+    const result = await db.collection('articles').findOneAndUpdate(
+        { name },
+        { $push: { comment: newComment } },
+        { returnDocument: 'after' }
+    );
+
+    res.json(result.value);
+});
+
+async function start() {
+    await connectToDB();
+    app.listen(8000, function () {
+        console.log('server is up and running on Port 8000');
     });
-    req.json({ article });
+}
 
-
-});
-
-// Start the server
-app.listen(8000, function () {
-    console.log('Server is up and running on port 8000');
-});
+start();
